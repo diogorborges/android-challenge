f62d1bd96173ed85f1e4391dee8fa5d64b71bd82
“Diogo <diogo.borges@walmartlabs.com>
2019-05-16 02:45:39 -0300

    Create Got Service

---
+++ app/src/main/java/es/npatarino/android/gotchallenge/data/remote/GotService.kt
@@ -0,0 +1,15 @@
+package es.npatarino.android.gotchallenge.data.remote
+
+import es.npatarino.android.gotchallenge.data.model.character.Character
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface GotService {
+
+    companion object {
+        const val GET_CHARACTERS = "characters.json"
+    }
+
+    @GET(GET_CHARACTERS)
+    fun getCharacters(): Single<ArrayList<Character>>
+}
