4021fb59bee6a840adef9c5607d84575484d4669
“Diogo <diogo.borges@walmartlabs.com>
2019-05-16 02:45:22 -0300

    Fix MainActivity and navigation bar

---
+++ app/src/main/java/es/npatarino/android/gotchallenge/presentation/MainActivity.kt
@@ -0,0 +1,84 @@
+package es.npatarino.android.gotchallenge.presentation
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import es.npatarino.android.gotchallenge.GotApplication
+import es.npatarino.android.gotchallenge.R
+import es.npatarino.android.gotchallenge.presentation.characters.CharactersFragment
+import kotlinx.android.synthetic.main.activity_container.toolbar
+import kotlinx.android.synthetic.main.activity_main.navigation
+
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private var title: String = CharactersFragment.TITLE
+    private lateinit var fragment: Fragment
+
+    companion object {
+        const val FRAGMENT_KEY: String = "fragment"
+        private const val TITLE_KEY: String = "title"
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GotApplication.instance.applicationComponent.inject(this)
+
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+        navigation.setOnNavigationItemSelectedListener(this)
+
+        title = savedInstanceState?.getString(TITLE_KEY) ?: title
+
+        if (supportFragmentManager.findFragmentByTag(FRAGMENT_KEY) == null) {
+            changeFragment(CharactersFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_sources -> {
+                title = CharactersFragment.TITLE
+                fragment = CharactersFragment()
+            }
+//            R.id.navigation_news -> {
+//                title = NewsFragment.TITLE
+//                fragment = NewsFragment()
+//            }
+        }
+
+        toolbar.title = title
+        changeFragment(fragment)
+
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toolbar.title = title
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(
+                R.id.main_container, fragment,
+                FRAGMENT_KEY
+            )
+            .commit()
+    }
+
+    fun showBackButton(show: Boolean) = with(toolbar) {
+        when (show) {
+            true -> navigationIcon =
+                resources.getDrawable(R.drawable.ic_apps_white_24dp)
+            else -> navigationIcon = null
+        }
+    }
+}
